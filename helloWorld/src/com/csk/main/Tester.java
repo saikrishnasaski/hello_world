dd6ace092745096f479eea91b9738a1de5867c07
saikrishnasaski <saikrishnachitti19@gmail.com>
2019-03-31 18:00:44 +0530

    committing main method

---
+++ helloWorld/src/com/csk/main/Tester.java
@@ -0,0 +1,14 @@
+package com.csk.main;
+
+import java.util.logging.Logger;
+
+public class Tester {
+	
+	private static final Logger logger = Logger.getLogger("Tester");
+
+	public static void main(String[] args) {
+		logger.info("hello github");
+
+	}
+
+}
